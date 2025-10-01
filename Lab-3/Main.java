c147fb1fc5a6955539f634b492abfe61b47fba56
v-rod-1 <v.rod216@yahoo.com>
2025-10-01 19:23:23 -0400

    Add files via upload

---
+++ Lab-3/Main.java
@@ -0,0 +1,63 @@
+public class Main {
+  public static void main(String[] args) {
+    String text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    
+    String bestCase = "ABC";
+
+    // * Initialize startTime here
+    long startTime = System.nanoTime();
+    
+    int index = match(text, bestCase);
+    // * Initialize endTime here
+    long endTime = System.nanoTime();
+    // * Calculate elapsedTime here
+    long elapsedTime = endTime - startTime;
+    
+    if (index >= 0)
+      System.out.println("best-case input matched at index " + index);
+    else
+      System.out.println("best-case input unmatched");   
+
+    System.out.println("best-case elapsed time: " + elapsedTime); // Elapsed time in nanoseconds
+
+    String worstCase = "789";
+    
+    // * Initialize startTime here
+    startTime = System.nanoTime();
+
+    index = match(text, worstCase);
+    // * Initialize endTime here
+    endTime = System.nanoTime();
+    // * Calculate elapsedTime here
+    elapsedTime = endTime - startTime;
+    
+    if (index >= 0)
+      System.out.println("worst-case input matched at index " + index);
+    else
+      System.out.println("worst-case input unmatched");
+    
+    System.out.println("worst-case elapsed time: " + elapsedTime); // Elapsed time in nanoseconds
+  }
+
+  // Return the index of the first match. -1 otherwise.
+  public static int match(String text, String pattern) {
+    for (int i = 0; i < text.length() - pattern.length() + 1; i++) {
+      if (isMatched(i, text, pattern))
+        return i;
+    }
+
+    return -1;
+  }
+	
+  // Test if pattern matches text starting at index i
+  private static boolean isMatched(int i, String text, 
+      String pattern) {
+    for (int k = 0; k < pattern.length(); k++) {
+      if (pattern.charAt(k) != text.charAt(i + k)) {
+        return false;
+      }
+    }
+    
+    return true;
+  }
+}
